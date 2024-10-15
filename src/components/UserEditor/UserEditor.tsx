cdee55701bc9c5eb75d255597ac7fb5c40f9f347
Irina Varushkina <rnvr@mail.ru>
2024-10-16 01:46:48 +0500

    Добавляет React Hook Form

--- src/components/UserEditor/UserEditor.tsx
+++ src/components/UserEditor/UserEditor.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import Button from '../Button/Button';
 import { User } from '../../types/types';
 import { editUser } from '../../features/usersSlice';
@@ -8,14 +9,15 @@ import { editUser } from '../../features/usersSlice';
 interface UserEditorProps {
     id: string | undefined;
 }
-const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
 
-    const [user, setUser] = useState<User | null>(null);
+const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
     const [isEditing, setIsEditing] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const { register, handleSubmit, setValue, formState: { errors } } = useForm<User>();
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -25,7 +27,15 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                setUser(data);
+                setValue('name', data.name);
+                setValue('username', data.username);
+                setValue('email', data.email);
+                setValue('address.street', data.address.street);
+                setValue('address.city', data.address.city);
+                setValue('address.zipcode', data.address.zipcode);
+                setValue('phone', data.phone);
+                setValue('website', data.website);
+                setValue('comment', data.comment);
             } catch (error) {
                 setError('Не удалось загрузить данные пользователя');
                 console.error('Fetch error:', error);
@@ -34,36 +44,14 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
             }
         };
 
-        if(id) {
+        if (id) {
             fetchUserData();
         }
-        fetchUserData();
-    }, [id]);
+    }, [id, setValue]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
-
-        if (name.startsWith('address.')) {
-            setUser((prevUser) => ({
-                ...prevUser!,
-                address: {
-                    ...prevUser!.address,
-                    [name.split('.')[1]]: value,
-                },
-            }));
-        } else {
-            setUser((prevUser) => ({
-                ...prevUser!,
-                [name]: value,
-            }));
-        }
-    };
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (user) {
-            dispatch(editUser(user));
-            navigate('/');
-        }
+    const onSubmit = (data: User) => {
+        dispatch(editUser(data));
+        navigate('/');
     };
 
     if (loading) {
@@ -73,21 +61,18 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
     if (error) {
         return <div>{error}</div>;
     }
-    if (!user) {
-        return <div>Пользователь не найден.</div>;
-    }
+
     return (
         <div>
             <h2>Профиль пользователя {id}</h2>
-            <Button onClick={() => setIsEditing(true)} title={'Редактировать'}/>
-            <form onSubmit={handleSubmit}>
+            <Button onClick={() => setIsEditing(true)} title={'Редактировать'} />
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label>Name:</label>
                     <input 
                         type='text' 
-                        name='name'
-                        value={user.name} 
-                        onChange={handleChange} 
+                        {...register('name', { required: true })} 
+                        style={{ borderColor: errors.name ? 'red' : undefined }} 
                         disabled={!isEditing} 
                     />
                 </div>
@@ -95,9 +80,8 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
                     <label>User Name:</label>
                     <input 
                         type='text' 
-                        name='username' 
-                        value={user.username} 
-                        onChange={handleChange} 
+                        {...register('username', { required: true })} 
+                        style={{ borderColor: errors.username ? 'red' : undefined }} 
                         disabled={!isEditing} 
                     />
                 </div>
@@ -105,9 +89,8 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
                     <label>Email:</label>
                     <input 
                         type='email' 
-                        name='email' 
-                        value={user.email} 
-                        onChange={handleChange} 
+                        {...register('email', { required: true })} 
+                        style={{ borderColor: errors.email ? 'red' : undefined }} 
                         disabled={!isEditing} 
                     />
                 </div>
@@ -115,9 +98,8 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
                     <label>Street:</label>
                     <input 
                         type='text' 
-                        name='address.street' 
-                        value={user.address.street} 
-                        onChange={handleChange} 
+                        {...register('address.street', { required: true })} 
+                        style={{ borderColor: errors.address?.street ? 'red' : undefined }} 
                         disabled={!isEditing} 
                     />
                 </div>
@@ -125,55 +107,49 @@ const UserEditor: React.FC<UserEditorProps> = ({ id }) => {
                     <label>City:</label>
                     <input 
                         type='text' 
-                        name='address.city' 
-                        value={user.address.city} 
-                        onChange={handleChange} 
-                        disabled={!isEditing} 
+                        {...register('address.city', { required: true })} 
+                        style={{ borderColor: errors.address?.city ? 'red' : undefined }}
+                        disabled={!isEditing}
                     />
                 </div>
                 <div>
                     <label>Zip Code:</label>
                     <input 
                         type='text' 
-                        name='address.zipcode' 
-                        value={user.address.zipcode} 
-                        onChange={handleChange} 
-                        disabled={!isEditing} 
+                        {...register('address.zipcode', { required: true })} 
+                        style={{ borderColor: errors.address?.zipcode ? 'red' : undefined }}
+                        disabled={!isEditing}
                     />
                 </div>
                 <div>
                     <label>Phone:</label>
                     <input 
-                        type='text'
-                        name='phone' 
-                        value={user.phone} 
-                        onChange={handleChange} 
-                        disabled={!isEditing} 
+                        type='text' 
+                        {...register('phone', { required: true })} 
+                        style={{ borderColor: errors.phone ? 'red' : undefined }}
+                        disabled={!isEditing}
                     />
                 </div>
                 <div>
                     <label>Website:</label>
                     <input 
                         type='text' 
-                        name='website'
-                        value={user.website} 
-                        onChange={handleChange} 
-                        disabled={!isEditing} 
+                        {...register('website', { required: true })}  
+                        style={{ borderColor: errors.website ? 'red' : undefined }}
+                        disabled={!isEditing}
                     />
                 </div>
                 <div>
                     <label>Comment:</label>
                     <textarea 
-                        name='comment' 
-                        value={user.comment || ''} 
-                        onChange={handleChange} 
-                        disabled={!isEditing} 
+                        {...register('comment')}
+                        disabled={!isEditing}
                     />
-                </div>
+                </div>       
                 <button type='submit' disabled={!isEditing}>Сохранить изменения</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default UserEditor;
